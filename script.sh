f579c2c82b176c387469adbae2d96509a5f8f169
pi-ender3 <pi>
2024-01-03 12:22:17 +0000

    New backup from 03-01-24

--- script.sh
+++ script.sh
@@ -35,6 +35,7 @@ git init
 git filter-branch --force --index-filter \
   'git rm -r --cached --ignore-unmatch "$parent_path"/.env' \
   --prune-empty --tag-name-filter cat -- --all
+
 #git rm -rf --cached "$parent_path"/.env
 git add "$parent_path"
 git commit -m "$commit_message"
